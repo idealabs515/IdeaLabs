db112cb2f19c40c88e73c5cab5da986c02dcd9f3
idealabs515 <41322531+idealabs515@users.noreply.github.com>
2018-08-07 18:24:11 +1000

    Enum name fixed in the whole class

--- src/FixBookControl.java
+++ src/FixBookControl.java
@@ -10,22 +10,22 @@ public class FixBookControl {
 
 	public FixBookControl() {
 		this.library = library.INSTANCE();
-		state = CONTROL_STATE.INITIALISED;
+		state = ControlState.INITIALISED;
 	}
 	
 	
 	public void setUI(FixBookUI fixBookUI) {
-		if (!state.equals(CONTROL_STATE.INITIALISED)) {
+		if (!state.equals(ControlState.INITIALISED)) {
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.fixBookUI = fixBookUI;
 		fixBookUI.setState(FixBookUI.UI_STATE.READY);
-		state = CONTROL_STATE.READY;		
+		state = ControlState.READY;		
 	}
 
 
 	public void bookScanned(int bookId) {
-		if (!state.equals(CONTROL_STATE.READY)) {
+		if (!state.equals(ControlState.READY)) {
 			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
 		}	
 		currentBook = library.Book(bookId);
@@ -40,12 +40,12 @@ public class FixBookControl {
 		}
 		fixBookUI.display(currentBook.toString());
 		fixBookUI.setState(FixBookUI.UI_STATE.FIXING);
-		state = CONTROL_STATE.FIXING;		
+		state = ControlState.FIXING;		
 	}
 
 
 	public void fixBook(boolean fix) {
-		if (!state.equals(CONTROL_STATE.FIXING)) {
+		if (!state.equals(ControlState.FIXING)) {
 			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
 		}	
 		if (fix) {
@@ -53,12 +53,12 @@ public class FixBookControl {
 		}
 		currentBook = null;
 		fixBookUI.setState(FixBookUI.UI_STATE.READY);
-		state = CONTROL_STATE.READY;		
+		state = ControlState.READY;		
 	}
 
 	
 	public void scanningComplete() {
-		if (!state.equals(CONTROL_STATE.READY)) {
+		if (!state.equals(ControlState.READY)) {
 			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
 		}	
 		fixBookUI.setState(FixBookUI.UI_STATE.COMPLETED);		
