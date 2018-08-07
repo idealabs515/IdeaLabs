d5231d31ca8988cfaeefcd272343e0914f1698a6
idealabs515 <idealabs515@gmail.com>
2018-08-07 21:43:36 +1000

    variable naming changed according to naming convention

--- src/book.java
+++ src/book.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class book implements Serializable {
 	
+	//T renamed to title
 	private String T;
+	//A renamed to author
 	private String A;
+	//C renamed to callNo
 	private String C;
+	//ID renamed to id
 	private int ID;
 	
 	private enum STATE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
@@ -14,30 +18,40 @@ public class book implements Serializable {
 	
 	
 	public book(String author, String title, String callNo, int id) {
-		this.A = author;
-		this.T = title;
-		this.C = callNo;
-		this.ID = id;
+		//A renamed to author
+		this.author = author;
+		//T renamed to title
+		this.title = title;
+		//C renamed to callNo
+		this.callNo = callNo;
+		//ID renamed to id
+		this.id = id;
 		this.state = STATE.AVAILABLE;
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(ID).append("\n")
-		  .append("  Title:  ").append(T).append("\n")
-		  .append("  Author: ").append(A).append("\n")
-		  .append("  CallNo: ").append(C).append("\n")
-		  .append("  State:  ").append(state);
+		//ID renamed to id
+		sb.append("Book: ").append(id).append("\n")
+			//T renamed to title
+			.append("  Title:  ").append(title).append("\n")
+			//A renamed to author
+			.append("  Author: ").append(author).append("\n")
+			//C renamed to callNo
+			.append("  CallNo: ").append(callNo).append("\n")
+			.append("  State:  ").append(state);
 		
 		return sb.toString();
 	}
 
 	public Integer ID() {
-		return ID;
+		//ID renamed to id
+		return id;
 	}
 
 	public String Title() {
-		return T;
+		//T renamed to title
+		return title;
 	}
 
 
@@ -67,10 +81,11 @@ public class book implements Serializable {
 		
 	}
 
-
-	public void Return(boolean DAMAGED) {
+	//DAMAGED renamed to isDamaged
+	public void Return(boolean isDamaged) {
 		if (state.equals(STATE.ON_LOAN)) {
-			if (DAMAGED) {
+			//DAMAGED renamed to isDamaged
+			if (isDamaged) {
 				state = STATE.DAMAGED;
 			}
 			else {
