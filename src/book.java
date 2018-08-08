4a5abf19bd32b0175af0fda9a0c3a54890153e4b
idealabs515 <idealabs515@gmail.com>
2018-08-08 15:52:48 +1000

    class naming changed according to naming convention

--- src/book.java
+++ src/book.java
@@ -2,6 +2,7 @@ import java.io.Serializable;
 
 
 @SuppressWarnings("serial")
+//book renamed to Book
 public class book implements Serializable {
 	
 	//T renamed to title
@@ -16,7 +17,7 @@ public class book implements Serializable {
 	private enum STATE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
 	private STATE state;
 	
-	
+	//book renamed to Book
 	public book(String author, String title, String callNo, int id) {
 		//A renamed to author
 		this.author = author;
