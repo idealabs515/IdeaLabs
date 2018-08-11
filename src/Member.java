a5ccdd3c37190037744f17ee5cbf86f4fcece0e6
idealabs515 <41322531+idealabs515@users.noreply.github.com>
2018-08-11 22:08:26 +1000

    Made few changes as per naming conventions for variables
    All variables such as name, id and loans did not have proper naimg conventions

--- src/Member.java
+++ src/Member.java
@@ -1,44 +1,49 @@
+/**
+Version-0.1
+@Editer:Chitty Vaishnav Reddy
+
+*/
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings("serial")
-public class member implements Serializable {
-
-	private String LN;
-	private String FN;
-	private String EM;
-	private int PN;
-	private int ID;
-	private double FINES;
+@SuppressWarnings("serial")//Classname starts with capital-case
+public class Member implements Serializable {
+    //Edited as naming conventions make no sense
+	private String lastName;
+	private String firstName;
+	private String email;
+	private int phoneNo;
+	private int id;
+	private double fines;
 	
-	private Map<Integer, loan> LNS;
+	private Map<Integer, loan> loans;
 
 	
-	public member(String lastName, String firstName, String email, int phoneNo, int id) {
-		this.LN = lastName;
-		this.FN = firstName;
-		this.EM = email;
-		this.PN = phoneNo;
-		this.ID = id;
+	public Member(String lastName, String firstName, String email, int phoneNo, int id) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.email = email;
+		this.phoneNo = phoneNo;
+		this.id = id;
 		
-		this.LNS = new HashMap<>();
+		this.loans = new HashMap<>();
 	}
 
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Member:  ").append(ID).append("\n")
-		  .append("  Name:  ").append(LN).append(", ").append(FN).append("\n")
-		  .append("  Email: ").append(EM).append("\n")
-		  .append("  Phone: ").append(PN)
+		sb.append("Member:  ").append(id).append("\n")
+		  .append("  Name:  ").append(lastName).append(", ").append(firstName).append("\n")
+		  .append("  Email: ").append(email).append("\n")
+		  .append("  Phone: ").append(phoneNo)
 		  .append("\n")
-		  .append(String.format("  Fines Owed :  $%.2f", FINES))
+		  .append(String.format("  Fines Owed :  $%.2f", fines))
 		  .append("\n");
 		
-		for (loan loan : LNS.values()) {
+		for (Loan loan : LNS.values()) {
 			sb.append(loan).append("\n");
 		}		  
 		return sb.toString();
