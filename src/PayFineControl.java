9e8de4b6f1993d3f17130ea1203db7a9e4f2dd5a
Bikram Shrestha <idealabs515@gmail.com>
2018-08-16 00:03:44 +1000

    Change in Object type
    As Library class and Member class has been updated with capital letter, the change was reflected.

--- src/PayFineControl.java
+++ src/PayFineControl.java
@@ -4,28 +4,28 @@ public class PayFineControl {
 	private enum ControlState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED }; //CONTROL_STATE changed to ControlState
 	private ControlState state;
 	
-	private library library;
-	private member member;;
+	private Library library;	//library changed to Library as Class name has been renamed.
+	private Member member;;		// member changed to Member as Class name has been renamed.
 
 
 	public PayFineControl() {
 		this.library = library.INSTANCE();
-		state = ControlState.INITIALISED;
+		state = ControlState.INITIALISED; //CONTROL_STATE changed to ControlState
 	}
 	
 	
 	public void setUI(PayFineUI ui) {
-		if (!state.equals(ControlState.INITIALISED)) {
+		if (!state.equals(ControlState.INITIALISED)) { //CONTROL_STATE changed to ControlState
 			throw new RuntimeException("PayFineControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.ui = ui;
 		ui.setState(PayFineUI.UI_STATE.READY);
-		state = ControlState.READY;		
+		state = ControlState.READY;		//CONTROL_STATE changed to ControlState
 	}
 
 
 	public void cardSwiped(int memberId) {
-		if (!state.equals(ControlState.READY)) {
+		if (!state.equals(ControlState.READY)) { //CONTROL_STATE changed to ControlState
 			throw new RuntimeException("PayFineControl: cannot call cardSwiped except in READY state");
 		}	
 		member = library.getMember(memberId);
@@ -36,18 +36,18 @@ public class PayFineControl {
 		}
 		ui.display(member.toString());
 		ui.setState(PayFineUI.UI_STATE.PAYING);
-		state = ControlState.PAYING;
+		state = ControlState.PAYING; //CONTROL_STATE changed to ControlState
 	}
 	
 	
 	public void cancel() {
 		ui.setState(PayFineUI.UI_STATE.CANCELLED);
-		state = ControlState.CANCELLED;
+		state = ControlState.CANCELLED; //CONTROL_STATE changed to ControlState
 	}
 
 
 	public double payFine(double amount) {
-		if (!state.equals(ControlState.PAYING)) {
+		if (!state.equals(ControlState.PAYING)) { //CONTROL_STATE changed to ControlState
 			throw new RuntimeException("PayFineControl: cannot call payFine except in PAYING state");
 		}	
 		double change = member.payFine(amount);
@@ -56,7 +56,7 @@ public class PayFineControl {
 		}
 		ui.display(member.toString());
 		ui.setState(PayFineUI.UI_STATE.COMPLETED);
-		state = ControlState.COMPLETED;
+		state = ControlState.COMPLETED; //CONTROL_STATE changed to ControlState
 		return change;
 	}
 	
